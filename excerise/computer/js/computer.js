6bbb55dfb52b100e4f9155714b55aab7be6c0c42
dpdahal <dp48dahal@gmail.com>
2024-12-18 12:41:55 +0545

    if else practice

---
+++ excerise/computer/js/computer.js
@@ -0,0 +1,29 @@
+// let options=prompt("1.Dell(Rs:20000) \n 2.Toshiba(Rs:30000) \n 3.Mac(Rs:50000)")
+// if(options==1){
+//     let quantity=prompt("Enter the quantity")
+//     let total=20000*quantity
+//     let product_name ="Dell"
+//     alert("Total amount is "+total)
+// }else if (options==2){
+//     let quantity=prompt("Enter the quantity")
+//     let total=30000*quantity
+//     let product_name ="Toshiba"
+//     alert("Total amount is "+total)
+// }
+// else if(options==3){
+//     let quantity=prompt("Enter the quantity")
+//     let total=50000*quantity
+//     let product_name ="Mac"
+//     alert("Total amount is "+total)
+// }else{
+//     alert("Invalid option")
+// }
+
+// // 27k
+// // 5 - 10 k-s
+// // 5 price -> 15
+
+
+let username ='admin'
+
+document.getElementById("content").innerHTML = username
---
+++ excerise/day1/js/custom.js
@@ -0,0 +1,126 @@
+// let x=prompt("Enter the value of x")
+// let y=prompt("Enter the value of y")
+
+// if(x>y){
+//     console.log("x is greater than y")
+// }
+// else if (x==y){
+//     console.log("x is equal to y")
+// }else{
+//     console.log("y is greater than x")
+// }
+
+
+// let x=5;
+// let y=6;
+// let z=7;
+
+// if(x>y){
+//     if(x>z){
+//         console.log("x is greater than y and z")
+//     }else{
+//         console.log("z is greater than x and y")
+//     }
+// }else{
+//     if(y>z){
+//         console.log("y is greater than x and z")
+//     }else{
+//         console.log("z is greater than x and y")
+//     }
+// }
+
+// if(x>y && x>z){
+//     console.log("x is greater than y and z")
+// }else if(y>x && y>z){
+//     console.log("y is greater than x and z")
+// }else{
+//     console.log("z is greater than x and y")
+// }
+
+
+// let lang ='nep'
+
+// switch(lang){
+//     case 'nep':
+//         console.log("Nepali")
+//         break;
+//     case 'eng':
+//         console.log("English")
+//         break;
+//     case 'hindi':
+//         console.log("Hindi")
+//         break;
+//     default:
+//         console.log("Invalid language")
+//         break;
+
+// }
+
+
+// let x=6
+// let y=5
+// let z=2
+
+// if(x>y && x>z){
+//     if(y>z){
+//         console.log(x,y,z)
+//     }else{
+//         console.log(x,z,y)
+//     }
+// }else if(y>x && y>z){
+//     if(x>z){
+//         console.log(y,x,z)
+//     }else{
+//         console.log(y,z,x)
+//     }
+// }else{
+//     if(x>y){
+//         console.log(z,x,y)
+//     }else{
+//         console.log(z,y,x)
+//     }
+// }
+
+
+// let books=['javascriopt','python','php','java','c++']
+
+// let username = prompt("Enter username: ")
+// let password= prompt("Enter password: ")
+
+// if(username=="admin" && password=="admin"){
+//     console.log(books)
+// }else{
+//     alert("Invalid username or password")
+// }
+
+
+// let gender='male'
+
+
+// let u_name ="ram abc"
+// let age =34
+// let phone=1234567890
+
+// console.log("My name is "+u_name
+//     +""
+// )
+
+// console.log(`
+//     My name is ${u_name}
+//     My age is ${age}
+//     My phone number is ${phone}
+//     `)
+
+// let table_row=`
+//  <tr>
+//         <td>1</td>
+//         <td>ram</td>
+//         <td>ram@gmail.com</td>
+//         <td>pokhara</td>
+//     </tr>
+// `
+
+// document.write(table_row)
+
+
+// for,while,do while, nested, for in, for of
